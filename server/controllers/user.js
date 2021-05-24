ef27f09ba5f0eb044b393882f94d7176bd90475b
moniratna <moniratna.munshi123@gmail.com>
2021-05-24 22:36:16 +0530

    user register with mongo

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,14 +1,35 @@
-
+const User = require('../models/user');
 
 exports.auth = function(req,res){
 
 }
 
 exports.register = function(req,res){
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const passwordConfirmation = req.body.passwordConfirmation;
-
-    res.json({username, email});
+    const {username, email, password, passwordConfirmation} = req.body;
+    
+    if (!password || !email){
+        return res.status(422).send({errors: [{title:"Data missing", detail:"Provide email and password"}]});
+    }
+    if (password !== passwordConfirmation) {
+        return res.status(422).send({errors: [{title: 'invalid password', detail:"password not matched "}]});
+    }
+    User.findOne({email}, (err,existingUser)=>{
+        if (err){
+            return res.status(422).send({"mongoose":"will later"});
+        }
+        if(existingUser) {
+            return res.status(422).send({errors:[{title: 'invalid email', detail: "User with this email already exist!"}]});
+        }
+        const user = new User({
+            username,
+            email,
+            password
+        });
+        user.save((err)=>{
+            if(err) {
+                return res.status(422).send({"mongoose":"handle mongoose error in next"})
+            }
+            return res.json({'registered':true});
+        });
+    });
 }
