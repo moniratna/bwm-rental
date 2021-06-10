41a2705c8d4e324c6b79329a73095bd2fd37bfb7
Moniratna <moniratna.munshi123@gmail.com>
2021-06-11 00:04:59 +0530

    book rentals back

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -1,7 +1,8 @@
 const Booking = require('../models/bookings');
 const Rental = require('../models/rental');
+const User = require('../models/user');
 const { normalizeErrors } = require('../helpers/mongoose');
-
+const moment = require('moment');
 
 
 exports.createBooking = function(req,res) {
@@ -10,16 +11,45 @@ exports.createBooking = function(req,res) {
 
     const booking = new Booking({startAt, endAt,totalPrice, guests, days});
     Rental.findById(rental._id)
-          .populate('bookings')
-          .populate('user')
-          .exec(function(err, foundRental){
-              
-        if(err) {
-            return res.status(422).send({errors: normalizeErrors(err.errors)});
-            }
-        if(foundRental.user.id === user.id) {
-            return res.status(422).send({errors: [{title: 'Invalid User', detail: "cannot create booking on your own rental"}]});
-            }
-            return res.json({booking, foundRental});
-        })
+        .populate('bookings')
+        .populate('user')
+        .exec(function(err, foundRental) {
+
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors)});
+    }
+
+    // if (foundRental.user.id === user.id) {
+    //   return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create booking on your Rental!'}]});
+    // }
+            
+
+        if(isValidBooking(booking, foundRental)){
+            foundRental.bookings.push(booking);
+            foundRental.save();
+            booking.save();
+
+            return res.json({'created': true});
+        } else {
+            return res.status(422).send({errors:[{title: 'Invalid Booking!', detail: "choosen dates are already taken"}]})
+        }
+    })
 }
+function isValidBooking(proposedBooking, rental) {
+    let isValid = true;
+  
+    if (rental.bookings && rental.bookings.length > 0) {
+  
+      isValid = rental.bookings.every(function(booking) {
+        const proposedStart = moment(proposedBooking.startAt);
+        const proposedEnd = moment(proposedBooking.endAt);
+  
+        const actualStart = moment(booking.startAt);
+        const actualEnd = moment(booking.endAt);
+  
+        return ((actualStart < proposedStart && actualEnd < proposedStart) || (proposedEnd < actualEnd && proposedEnd < actualStart));
+      });
+    }
+  
+    return isValid;
+  }
