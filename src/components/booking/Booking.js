86b3e8317403ba8db462924e4cafec585b738a49
Moniratna <moniratna.munshi123@gmail.com>
2021-06-14 21:09:25 +0530

    booking date picker

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -8,8 +8,16 @@ export class Booking extends React.Component {
   constructor(){
     super();
     this.bookedOutDates = [];
+    this.dateRef = React.createRef();
+
+    this.state = {
+      startAt: '',
+      endAt: '',
+      guests: 0
+    }
 
     this.checkInvalidDates = this.checkInvalidDates.bind(this);
+    this.handleApply = this.handleApply.bind(this);
   }
   componentWillMount(){
     this.getBookedOutDates();
@@ -27,6 +35,26 @@ export class Booking extends React.Component {
   checkInvalidDates(date) {
     return this.bookedOutDates.includes(date.format('Y/MM/DD')) || date.diff(moment(), 'days') < 0;
   }
+  handleApply = (event, picker)=>{
+    const startAt = picker.startDate.format('Y/MM/DD');
+    const endAt = picker.endDate.format('Y/MM/DD');
+
+    this.dateRef.current.value = startAt + ' to ' + endAt;
+
+    this.setState({
+      startAt,
+      endAt
+    });
+    console.log();
+  }
+  selectGuests(e){
+    this.setState({
+      guests: parseInt(e.target.value)
+    })
+  }
+  reserve(){
+    console.log(this.state);
+  }
 
   render() {
     const {rental} = this.props;
@@ -36,15 +64,18 @@ export class Booking extends React.Component {
         <hr></hr>
         <div className='form-group'>
         <label htmlFor='dates'>Dates</label>
-          <DateRangePicker isInvalidDate={this.checkInvalidDates} opens='left' containerStyles={{display: 'block'}}>
-            <input id='dates' type='text' className='form-control'></input>
+          <DateRangePicker
+          onApply={this.handleApply}
+          isInvalidDate={this.checkInvalidDates} opens='left' containerStyles={{display: 'block'}}>
+            <input ref={this.dateRef} id='dates' type='text' className='form-control'></input>
           </DateRangePicker>
         </div>
         <div className='form-group'>
           <label htmlFor='guests'>Guests</label>
-          <input type='number' className='form-control' id='guests' aria-describedby='emailHelp' placeholder=''></input>
+          <input onChange={(e)=>{this.selectGuests(e)}} 
+          type='number' className='form-control' id='guests' aria-describedby='emailHelp' placeholder=''></input>
         </div>
-        <button className='btn btn-bwm btn-confirm btn-block'>Reserve place now</button>
+        <button onClick={()=>{this.reserve()}} className='btn btn-bwm btn-confirm btn-block'>Reserve place now</button>
         <hr></hr>
         <p className='booking-note-title'>People are interested into this house</p>
         <p className='booking-note-text'>
